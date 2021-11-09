a4a394ceacf6144e4734d52a5b234278ddbc2526
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2021-11-09 21:54:57 +0100

    Change package name

--- legal_documents.go
+++ legal_documents.go
@@ -2,7 +2,7 @@
 //
 // Copyright (C) 2018 - 2021 J&J Ideenschmiede GmbH <info@jj-ideenschmiede.de>
 //
-// This file is part of goerech24.
+// This file is part of goerecht24.
 // All code may be used. Feel free and maybe code something better.
 //
 // Author: Jonas Kwiedor (aka gowizzard)
